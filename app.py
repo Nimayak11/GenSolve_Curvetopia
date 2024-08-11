b7c54105c4508bb7f82d5e115daa0aad4eecab92
Navya <136168813+Nimayak11@users.noreply.github.com>
2024-08-11 23:54:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def process():
     
     return send_file(result_image_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
